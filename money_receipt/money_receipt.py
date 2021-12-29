ab1f6205a5974eac2aa1255844344446d63c889b
kazi <kazialamin109@gmail.com>
2021-12-29 14:27:26 +0600

    changes on mr

--- money_receipt/money_receipt.py
+++ money_receipt/money_receipt.py
@@ -74,6 +74,7 @@ class  MoneyReceipt(osv.osv):
             journal_vals["journal_id"]=res.journal_id.id
             journal_vals["period_id"]=res.period_id.id
             journal_vals["date"]=res.date
+            journal_vals["ref"]=res.address
 
             debit_temp_dict={'analytic_account_id': False,
                        'tax_code_id': False,
